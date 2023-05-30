fc35a2f56aebf93b5f0b27a2941635a33d4f4d26
ahmedhh33 <ahmedhh3633@gmail.com>
2023-05-30 11:20:16 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("My Name: ");
+        Console.WriteLine("Ahmed ");
+        Console.WriteLine("********************* ");
+        Console.Write("My Father Name: ");
+        Console.WriteLine("Humaid");
+        Console.WriteLine("********************* ");
+        Console.Write("My ID: ");
+        Console.WriteLine("23416871");
+        Console.WriteLine("********************* ");
+        Console.Write("My BD: ");
+        Console.WriteLine("6-12-1997");
+        Console.WriteLine("********************* ");
+
+    }
+}
